56b83cf7508c170047a981f1130aa242f49fa6b3
JungwonSeong <yoeubi28@gmail.com>
2019-05-12 00:29:06 +0900

    add: eslint and prettier

--- README.md
+++ README.md
@@ -3,3 +3,27 @@
 안녕하세요 `탈주닌자`팀입니다.
 
 저희 기본적으로 CRA(create-react-app)이 아닌 Parcel로 처음부터 모든 걸 설정하고 빌드했습니다.
+
+## Prettier 설정
+
+```
+{
+  문자열은 `'`로 사용
+  "singleQuote": true,
+	코드는 세미콜론으로 마무리
+  "semi": true,
+	탭 대신에 스페이스를 사용
+  "useTabs": false,
+	들여쓰기 크기는 2
+  "tabWidth": 2,
+	객체나 배열을 작성할때 마지막 원소 혹은 키에도 쉼표를 붙이는것
+  "trailingComma": "all",
+	한 줄이 80칸을 넘지 않도록
+  "printWidth": 80
+}
+
+```
+
+## 참고 사이트
+
+[Typescript](https://medium.com/@dors718/linting-your-react-typescript-project-with-eslint-and-prettier-2423170c3d42)
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import * as React from "react";
-import * as styles from "./App.scss";
+import * as React from 'react';
+import * as styles from './App.scss';
 
 interface Props {}
 
 interface State {
-    input: string;
+  input: string;
 }
 
 class App extends React.Component<Props, State> {
-    state = {
-        input: "test"
-    };
-    test = async () => {
-        await setTimeout(() => this.setState({ input: "test2" }), 1000);
-    };
-    render() {
-        const { input } = this.state;
-        console.log(input);
-        return (
-            <div className={styles.App}>
-                {input} <button onClick={this.test}>업</button>
-            </div>
-        );
-    }
+  state = {
+    input: 'test',
+  };
+  test = async () => {
+    await setTimeout(() => this.setState({ input: 'test2' }), 1000);
+  };
+  render() {
+    const { input } = this.state;
+    return (
+      <div className={styles.App}>
+        {input} <button onClick={this.test}>업</button>
+      </div>
+    );
+  }
 }
 
 export default App;
