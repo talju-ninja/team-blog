45b976cbc263f148a7df7c369406881547413a4e
JungwonSeong <yoeubi28@gmail.com>
2019-05-12 16:54:11 +0900

    feat: add react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root') as HTMLElement,
+);
