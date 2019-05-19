70f1be66d309f2be308ef711c9d9a29991d31101
JungwonSeong <yoeubi28@gmail.com>
2019-05-19 17:47:04 +0900

    feat: layout component

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,98 @@
+import * as React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+
+const styles = (theme: Theme) => ({
+  card: {
+    margin: '50px 0',
+    width: '100%',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  actions: {
+    display: 'flex',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+});
+
+interface Props {
+  classes: {
+    card: string;
+    media: string;
+    actions: string;
+    avatar: string;
+  };
+}
+
+interface State {
+  expanded: boolean;
+}
+
+class PostItem extends React.Component<Props, State> {
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader={new Date().toLocaleTimeString()}
+        />
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/paella.jpg"
+          title="Paella dish"
+        />
+        <CardContent>
+          <Typography component="p">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.actions} disableActionSpacing>
+          <IconButton aria-label="Add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="Share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+export default withStyles(styles)(PostItem);
---
+++ src/components/PostList.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import PostItem from './PostItem';
+import Layout from '../layout/Layout';
+
+const PostList = () => {
+  return (
+    <Layout>
+      <PostItem />
+      <PostItem />
+      <PostItem />
+      <PostItem />
+      <PostItem />
+      <PostItem />
+      <PostItem />
+      <PostItem />
+      <PostItem />
+      <PostItem />
+    </Layout>
+  );
+};
+
+export default PostList;
