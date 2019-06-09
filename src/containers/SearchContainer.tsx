fe1818ec98b2f97f7c8cb3e9f7130b5c5df20caf
shiincs <skywing27@gmail.com>
2019-06-09 22:35:24 +0900

    feat: add Search components

---
+++ src/containers/SearchContainer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import Search from '../components/Search/Search';
+
+interface Props {}
+interface State {}
+
+export default class SearchContainer extends React.Component<Props, State> {
+  render() {
+    return <Search />;
+  }
+}
