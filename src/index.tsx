1302b29005ddde47135baf8ad8b9ac10ff870ff6
JungwonSeong <yoeubi28@gmail.com>
2019-06-09 17:39:43 +0900

    merge

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import Amplify from 'aws-amplify';
-import config from './aws-exports';
+import config from './aws-exports.js';
 
 Amplify.configure(config);
 
