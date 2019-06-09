b7ca87723a86769c271c0f68861ba061dc6b8b15
shiincs <skywing27@gmail.com>
2019-06-09 22:35:05 +0900

    feat: add ListItem component

---
+++ src/components/ListItem/ListItem.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { withStyles, createStyles } from '@material-ui/styles';
+import { WithStyles } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import defaultImage from '../../../assets/images/default-image.png';
+
+const styles = () =>
+  createStyles({
+    dl: {
+      width: '70%',
+      '@media (max-width: 414px)': {
+        width: '100%',
+      },
+      margin: '0 auto',
+      border: '1px solid red',
+    },
+    dt: {
+      fontSize: 30,
+      '@media (max-width: 414px)': {
+        fontSize: 24,
+      },
+      fontWeight: 600,
+    },
+    dd: {
+      margin: 0,
+      fontSize: 18,
+      '@media (max-width: 414px)': {
+        fontSize: 14,
+      },
+    },
+  });
+
+interface Props extends WithStyles<string> {}
+
+const ListItem: React.SFC<Props> = (props: React.PropsWithChildren<Props>) => {
+  const { classes } = props;
+  return (
+    <>
+      <CssBaseline />
+      <li>
+        <dl className={classes.dl}>
+          <dt className={classes.dt}>여기에 글 제목이 와야됩니다.</dt>
+          <dd className={classes.dd}>
+            여기에 글 내용이 와야됩니다. 블라블라블라...
+          </dd>
+          <dd className={classes.dd}>2019.06.09</dd>
+          <dd className={classes.dd}>나루토</dd>
+        </dl>
+      </li>
+    </>
+  );
+};
+
+export default withStyles(styles)(ListItem);
