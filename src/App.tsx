aefa52c15fe82489f585e277f685a16de29cdc18
JungwonSeong <yoeubi28@gmail.com>
2019-06-23 18:13:49 +0900

    solove conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import AboutTeamPage from './pages/AboutTeamPage';
 import SearchContainer from './containers/SearchContainer';
@@ -12,13 +12,11 @@ interface State {}
 class App extends React.Component<Props, State> {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/about/team" component={AboutTeamPage} />
-          <Route path="/search" component={SearchContainer} />
-          <Route path="/" component={PostListContainer} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/about/team" component={AboutTeamPage} />
+        <Route path="/search" component={SearchContainer} />
+        <Route path="/" component={PostListContainer} />
+      </Switch>
     );
   }
 }
