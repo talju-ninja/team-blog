d8fbb207a22e0a0ebcc76c7be15f1874423089ce
JungwonSeong <yoeubi28@gmail.com>
2019-06-23 15:43:24 +0900

    postItem update

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,100 +1,50 @@
 import * as React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import red from '@material-ui/core/colors/red';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import CardHeader from '@material-ui/core/CardHeader';
 
-const styles = (theme: Theme) => ({
+const styles = {
   card: {
     margin: '50px 0',
     width: '100%',
   },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  actions: {
-    display: 'flex',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-});
+};
 
 interface Props {
   classes: {
     card: string;
-    media: string;
-    actions: string;
-    avatar: string;
   };
 }
 
-interface State {
-  expanded: boolean;
-}
+const PostItem: React.FC<Props> = props => {
+  const { classes } = props;
 
-class PostItem extends React.Component<Props, State> {
-  state = { expanded: false };
-
-  handleExpandClick = () => {
-    this.setState(state => ({ expanded: !state.expanded }));
-  };
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <Card className={classes.card}>
-        {/* <CardHeader
-          avatar={
-            <Avatar aria-label="Recipe" className={classes.avatar}>
-              유저
-            </Avatar>
-          }
-          action={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="블로그 제목"
-          subheader={new Date().toLocaleTimeString()}
-        /> */}
-        <CardMedia
-          className={classes.media}
-          image="https://cdn-images-1.medium.com/max/1600/1*HP8l7LMMt7Sh5UoO1T-yLQ.png"
-          title="Paella dish"
-        />
-        <CardContent>
-          <Typography component="p">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
-          </Typography>
-        </CardContent>
-        {false && (
-          <CardActions className={classes.actions} disableActionSpacing>
-            <IconButton aria-label="Add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="Share">
-              <ShareIcon />
-            </IconButton>
-          </CardActions>
-        )}
-      </Card>
-    );
-  }
-}
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        title="블로그 제목"
+        subheader={new Date().toLocaleTimeString()}
+      />
+      <CardContent>
+        <Typography component="p" noWrap>
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like. This impressive paella is a perfect party dish
+          and a fun meal to cook together with your guests. Add 1 cup of frozen
+          peas along with the mussels, if you like. This impressive paella is a
+          perfect party dish and a fun meal to cook together with your guests.
+          Add 1 cup of frozen peas along with the mussels, if you like. This
+          impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like. This impressive paella is a perfect party dish
+          and a fun meal to cook together with your guests. Add 1 cup of frozen
+          peas along with the mussels, if you like.
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default withStyles(styles)(PostItem);
